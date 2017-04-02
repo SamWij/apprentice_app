084e74ea1159c328bdaa89e893d64388a2818cfa
SamWij <sam.wijesinha@gmail.com>
2017-04-02 18:16:16 +1000

    user signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to Apprenticeships!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:firstname, :surname, :email, :password, :password_confirmation)
   end
 end
