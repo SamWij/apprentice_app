405b4b5c653509b312f5ca1330e2483012488a85
SamWij <sam.wijesinha@gmail.com>
2017-04-02 17:29:26 +1000

    made basic user model and secure password

---
+++ test/models/user_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+end
